480b7784bdef2e23213d333ac3cc209885cc586e
Lucciola <hidenari.t1997nov@gmail.com>
2019-04-01 03:54:37 +0900

    apply: camera fragment

--- sample/src/main/java/com/clockvoid/sample/MainActivity.kt
+++ sample/src/main/java/com/clockvoid/sample/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.cameraFragment, CameraFragment())
+        fragmentTransaction.commit()
     }
 }
