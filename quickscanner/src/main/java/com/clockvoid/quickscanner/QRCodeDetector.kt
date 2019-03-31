e151ec211158da797c109cd004738360cfd4d0e4
Lucciola <hidenari.t1997nov@gmail.com>
2019-04-01 02:45:42 +0900

    fix: qrcode detector

--- quickscanner/src/main/java/com/clockvoid/quickscanner/QRCodeDetector.kt
+++ quickscanner/src/main/java/com/clockvoid/quickscanner/QRCodeDetector.kt
@@ -1,6 +1,5 @@
 package com.clockvoid.quickscanner
 
-import android.graphics.Bitmap
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions
@@ -17,20 +16,10 @@ object QRCodeDetector {
     private val detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options)
 
     fun detect(
-        bitmap: Bitmap,
-        onSuccess: (images: List<FirebaseVisionBarcode>) -> Unit,
-        onFailure: (func: Exception) -> Unit
-    ) {
-        val firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap)
-        detect(firebaseVisionImage, onSuccess, onFailure)
-    }
-
-    fun detect(
         image: FirebaseVisionImage,
         onSuccess: (images: List<FirebaseVisionBarcode>) -> Unit,
         onFailure: (func: Exception) -> Unit
     ) {
-
         detector
             .detectInImage(image)
             .addOnSuccessListener(onSuccess)
